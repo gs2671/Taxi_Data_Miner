90184d2b0916a42188ef9f52b7fd053f6f3b3ebc
vignesh6v <vignesh.6v@gmail.com>
2016-04-29 01:13:50 -0400

    Distinct records
    To get hourly unique weather conditions.

---
+++ Testing/Weather_vs_Trips/Step_0_Wdata/unique_conditions_and_hours.py
@@ -0,0 +1,27 @@
+import os
+import time
+import pandas as p
+
+
+
+def read_data(file_name):
+
+    file = file_name+'.csv'
+    data_frame = p.read_csv(file)
+    data_frame = data_frame[['Time','Conditions',]]
+
+    #remove dupdiacte if exists
+    try:
+        data_frame = data_frame.drop_duplicates()
+    except:
+        pass
+    data_frame = data_frame.set_index('Time')
+    return data_frame
+
+
+def main():
+	file = 'weather_2015'
+	data_frame = read_data(file)
+	data_frame.to_csv('2015_Weather_Condition.csv', sep=',')
+
+if __name__=='__main__':main()
